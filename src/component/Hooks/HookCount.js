d2e01a8dd637c5da6ad4f228a1a30b88af76dc58
Manish Rana <alertmanishrana@gmail.com>
2020-05-01 10:01:20 +0545

    dfbew

--- src/component/Hooks/HookCount.js
+++ src/component/Hooks/HookCount.js
@@ -1,13 +1,39 @@
-import React,{useState} from "react"
+import React,{useState, useEffect} from "react";
+import randomcolor from "randomcolor";
 
 // Convert the class below to a functional component that uses the useState hook to initalize a count vartiable to 0 and display the count on the screen.
 // Don't worry about the part where the button changes the count quite yet, that's what you're here to learn about!
 
 
+// for useEffect
+// componentDidMount
+// componentDidUpdate
+// componentWillUnmount
+
+// Side effects?
+// Network request
+// Manual DOM manipulation
+// Event listeners or timeouts and intervals
 
 function HookCount(){
-    const [count, setCount] = useState(0);
-    const style={textAlign:'center'};
+        const [count, setCount] = useState(0);
+        // use random color only once on load
+        // const [color, setColor] = useState(randomcolor());
+        const [color, setColor] = useState("");
+        const style={textAlign:'center'};
+    
+
+    // use random color only once on load
+    // useEffect(()=>{
+    //     setColor(randomcolor());
+    // },[])
+
+    useEffect(()=>{
+        setColor(randomcolor());
+        setInterval(()=>{
+            console.log('Interval');
+        },1000)
+    },[count])
 
     function increment(){
         setCount(prevCount=>prevCount+1);
@@ -27,13 +53,11 @@ function HookCount(){
 
     return(
       <div style={style}>
-        <h1>{count}</h1>
+        <h1 style={{color:color}}>{count}</h1>
         <button onClick={increment}>Increment!</button>
         <button onClick={decrement}>Decrement!</button>
         <button onClick={double}>Double!</button>
         <button onClick={half}>Half!</button>
-
-
       </div>  
     );
 }
